132871fb4ef6ef463be7d6f2c40c37ac1fc204c4
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-11-14 17:08:05 +0400

    написал первые три модели

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,46 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Subject(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+class Course(models.Model):
+    owner = models.ForeignKey(User,
+                              related_name='courses_created',
+                              on_delete=models.CASCADE,
+                              verbose_name='Преподаватель')
+    subject = models.ForeignKey(Subject,
+                                related_name='courses',
+                                on_delete=models.CASCADE,
+                                verbose_name='Категория(Предмет)')
+    title = models.CharField(max_length=200, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField(verbose_name='Краткое описание')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+
+class Module(models.Model):
+    course = models.ForeignKey(Course,
+                               related_name='modules',
+                               on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, verbose_name='Название')
+    description = models.TextField(blank=True, verbose_name='Описание')
+
+
+    def __str__(self):
+        return self.title
