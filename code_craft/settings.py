e2196d6191e6703c929c8577db2c056403066522
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-11-14 17:16:07 +0400

    настроил базу данных Mysql

--- code_craft/settings.py
+++ code_craft/settings.py
@@ -71,12 +71,15 @@ WSGI_APPLICATION = 'code_craft.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": str(os.getenv("DB_NAME")),
+        "USER": str(os.getenv("DB_USER")),
+        "PASSWORD": str(os.getenv("DB_PASSWORD")),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
