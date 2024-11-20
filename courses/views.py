a9399948e2ec57119770b853d01d413263d2bb9c
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-11-20 17:54:07 +0400

    добавил вью классы для crud операций

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from courses.services.mixins import OwnerCourseMixin, OwnerCourseEditMixin
 
-# Create your views here.
+
+class ManageCourseListView(PermissionRequiredMixin, OwnerCourseMixin, ListView):
+    template_name = 'course/list.html'
+    permission_required = 'courses.view_course'
+
+
+class CourseCreateView(PermissionRequiredMixin, OwnerCourseEditMixin, CreateView):
+    permission_required = 'courses.add_course'
+
+
+class CourseUpdateView(PermissionRequiredMixin, OwnerCourseEditMixin, UpdateView):
+    permission_required = 'courses.change_course'
+
+
+class CourseDeleteView(PermissionRequiredMixin, OwnerCourseMixin, DeleteView):
+    template_name = 'course/delete.html'
+    permission_required = 'courses.delete_course'
+
