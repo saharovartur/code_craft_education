c9681343504322efc59e71ec177e95083baaa5d6
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-11-20 17:51:32 +0400

    Добавил несколько классов-миксинов для повторного использования в своих вью

---
+++ courses/services/mixins.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+
+from courses.models import Course
+
+
+class OwnerMixin:
+    """Миксин автора курса"""
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+
+
+class OwnerEditMixin:
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin):
+    model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    template_name = 'course/form.html'
