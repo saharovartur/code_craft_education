3cacd3cc73ac59ff3edbbc89207e69fbab2ad091
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-11-20 17:52:04 +0400

    Добавил путь до новой папки с templates

--- code_craft/settings.py
+++ code_craft/settings.py
@@ -51,7 +51,8 @@ ROOT_URLCONF = 'code_craft.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates',
+                 BASE_DIR / 'templates/registration',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
