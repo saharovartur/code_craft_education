9b66d9582516dd87fe1141a0cad90f891d6585ed
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-11-20 10:56:11 +0400

    добавил доп.поля verbose_name в модели

--- courses/models.py
+++ courses/models.py
@@ -1,19 +1,25 @@
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
 class Subject(models.Model):
+    """Модель предмета"""
     title = models.CharField(max_length=200, verbose_name='Название')
     slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
         ordering = ['title']
+        verbose_name = 'Предмет'
+        verbose_name_plural = 'Предметы'
 
     def __str__(self):
         return self.title
 
 
 class Course(models.Model):
+    """Модель курса"""
     owner = models.ForeignKey(User,
                               related_name='courses_created',
                               on_delete=models.CASCADE,
@@ -29,18 +35,69 @@ class Course(models.Model):
 
     class Meta:
         ordering = ['-created']
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
 
     def __str__(self):
         return self.title
 
 
 class Module(models.Model):
+    """Модель модуля"""
     course = models.ForeignKey(Course,
                                related_name='modules',
                                on_delete=models.CASCADE)
     title = models.CharField(max_length=200, verbose_name='Название')
     description = models.TextField(blank=True, verbose_name='Описание')
 
+    class Meta:
+        verbose_name = 'Модуль'
+        verbose_name_plural = 'Модули'
 
     def __str__(self):
         return self.title
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents',
+                               on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,
+                                     on_delete=models.CASCADE,
+                                     limit_choices_to={'model__in':(
+                                         'text',
+                                         'video',
+                                         'image',
+                                         'file')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, related_name='%(class)s_relates', on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+
+
+class Video(ItemBase):
+    url = models.URLField()
+
+
