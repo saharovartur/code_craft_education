259d9484309ab35d8578af9234e05d6be08387d4
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-11-26 17:36:06 +0400

    Добавил viewset для обработки списка курсов(+детальная инфо) + view для обработки записи на курс

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,13 +1,37 @@
-from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from courses.api.serializers import SubjectSerializer
-from courses.models import Subject
+from courses.api.serializers import SubjectSerializer, CourseSerializer
+from courses.models import Subject, Course
 
 
-class SubjectListView(generics.ListAPIView):
+class SubjectListView(generics.ListCreateAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
 
-class SubjectDetailView(generics.RetrieveAPIView):
+
+class SubjectDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    """Список курсов + детальная инфо о курсе"""
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class CourseEnrollView(APIView):
+    """Запись на курс"""
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
