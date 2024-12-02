60121b59912d7ee25ef3504c4a01d1f79c395548
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-12-02 17:46:20 +0400

    Добавил вью для обработки возможности присоединения к чату курса студенту курса

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+def course_chat_room(request, course_id):
+    try:
+        course = request.user.courses_joined.get(id=course_id) # извлечь курс с заданным id к которому присоединился текущий юзер
+    except:
+        return HttpResponseForbidden()  # юзер не студент курса или курса не существует
+    return render(request, 'chat/room.html', {'course': course})
+
