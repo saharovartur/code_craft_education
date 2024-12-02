73092fa8c8dfa36a8a58979abe39f91a7cd5f397
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-12-02 13:25:16 +0400

    Добавил метод во вью CourseViewSet для обработки запроса курсов с контентом

--- courses/api/views.py
+++ courses/api/views.py
@@ -3,7 +3,9 @@ from rest_framework.authentication import BasicAuthentication
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from courses.api.serializers import SubjectSerializer, CourseSerializer
+
+from courses.api.permissions import IsEnrolled
+from courses.api.serializers import SubjectSerializer, CourseSerializer, CourseWithContentsSerializer
 from courses.models import Subject, Course
 
 
@@ -18,7 +20,7 @@ class SubjectDetailView(generics.RetrieveUpdateDestroyAPIView):
 
 
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
-    """Список курсов + детальная инфо о курсе"""
+    """Список курсов + детальная инфо о курсе + запись на курс"""
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
@@ -31,6 +33,19 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+    @action(detail=True,
+            methods=['get'],
+            serializer_class=CourseWithContentsSerializer,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+
+
+
+
 
 ## Первоначальная версия кода записи на курс
 # class CourseEnrollView(APIView):
