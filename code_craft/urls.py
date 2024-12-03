2ce541992ba23165fe322541560a4f9ea39b4737
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-12-03 10:15:54 +0400

    добавил урл для страницы с чатом

--- code_craft/urls.py
+++ code_craft/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('students/', include('students.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
     path('api/', include('courses.api.urls', namespace='api')),
+    path('chat/', include('chat.urls', namespace='chat')),
 
 ]
 
