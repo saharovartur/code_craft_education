d9d99bd65de4e92649d9400eda1c497d721cb159
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-12-03 16:17:35 +0400

    добавил настройки asgi

--- code_craft/asgi.py
+++ code_craft/asgi.py
@@ -9,8 +9,19 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'code_craft.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(chat.routing.websocket_urlpatterns)
+    )
+})
