79011ad80a0627b28147e9f55c68380689db9d90
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-12-03 10:16:21 +0400

    добавил док-стринг для вьюхи

--- chat/views.py
+++ chat/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 
 @login_required
 def course_chat_room(request, course_id):
+    """Вью для предоставления доступа к чату
+    курса на который студент был записан"""
     try:
         course = request.user.courses_joined.get(id=course_id) # извлечь курс с заданным id к которому присоединился текущий юзер
     except:
