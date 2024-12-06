6dce6852c6d27a786b882707cb0f77c9f0db81da
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-12-06 11:56:14 +0400

    добавил кастомный middleware

--- code_craft/settings/base.py
+++ code_craft/settings/base.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'courses.services.middleware.subdomain_course_middleware',
 ]
 
 ROOT_URLCONF = 'code_craft.urls'
