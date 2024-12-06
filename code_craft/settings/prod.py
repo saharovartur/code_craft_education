9dedcd998d454091afba02378c653f0dc07fad5d
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-12-06 10:27:34 +0400

    Добавил настройки в файл настроек  версию прода

--- code_craft/settings/prod.py
+++ code_craft/settings/prod.py
@@ -1,15 +1,31 @@
-from code_craft.settings.base import *
+from .base import *
 
 DEBUG = False
+
 
 ADMINS = [
     ('Artur S', 'geek_ant@email.com'),
 ]
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['codecraft.com', 'www.codecraft.com', '']
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'POST': 5432,
 
     }
 }
+
+REDIS_URL = 'redis://cache:6379'
+CACHES['default']['LOCATION'] = REDIS_URL
+CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
+
+# Безопасность
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
